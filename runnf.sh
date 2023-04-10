40f5a89449487aa04530f48f23a51c99f3f8629f
Arif YILMAZ <73948166+arifx@users.noreply.github.com>
2023-04-10 16:35:47 +0200

    run nf

---
+++ runnf.sh
@@ -0,0 +1,1 @@
+nextflow run ./processes/ProcessKG.nf --input_path ./processes/ENOSE.xlsx --file_name KG.json --project_dir ./processes/ --output_path ./processes  -with-docker snheffer/nextflow:kg-latest
