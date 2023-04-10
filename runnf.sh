101937cc81483162c8b62b457f33401d1320a5a5
Arif YILMAZ <73948166+arifx@users.noreply.github.com>
2023-04-10 17:38:53 +0200

    s

--- runnf.sh
+++ runnf.sh
@@ -2,4 +2,5 @@ git pull
 
 nextflow run ./processes/ProcessKG.nf --input_path ./processes/ENOSE.xlsx --file_name KG.json --project_dir ./processes/ --output_path ./processes 
 #nextflow run ./processes/ProcessKG.nf --input_path ./processes/ENOSE.xlsx --file_name KG.json --project_dir ./processes/ --output_path ./processes  -with-docker snheffer/nextflow:kg-latest
+
 cat processes/KG.json
