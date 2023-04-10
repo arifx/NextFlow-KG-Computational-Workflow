d48d7ea4f105ea7cdbdebc31917998bccb19b320
Arif YILMAZ <73948166+arifx@users.noreply.github.com>
2023-04-10 17:35:32 +0200

    s

--- processes/ProcessENOSE2KG.py
+++ processes/ProcessENOSE2KG.py
@@ -7,19 +7,20 @@ g = Graph()
 wb = load_workbook('/home/appuser/NextFlow-KG-Computational-Workflow/ENOSE.xlsx')
 sheet = wb['Sheet1']
 if 1==1:
-    row= sheet[1]
+    row= list(sheet.rows)[0]
     print(str(row))
 #for row in sheet.iter_rows(min_row=2, values_only=True):
-    subject_uri = row[0]
-    subject_name = row[1]
+    subject_uri = row[0].value
+    subject_name = row[1].value
     subject = ex[subject_uri]
     g.add((subject, RDF.type, ex[row[0]]))
     g.add((subject, ex['hasName'], Literal(subject_name)))
 
     for i in range(2, len(row)):
         if row[i]:
-            predicate = ex[f'hasMeasurementType{i-1}']
-            object_uri = row[i]
+            #predicate = ex[f'hasMeasurementType{i-1}']
+            predicate = ex[f'hasMeasurementType']
+            object_uri = row[i].value
             object = ex[object_uri]
             g.add((subject, predicate, object))
 
