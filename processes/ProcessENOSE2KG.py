0309df2973e93b778706849689995e9dc029d6ed
Arif YILMAZ <73948166+arifx@users.noreply.github.com>
2023-04-10 17:57:04 +0200

    s

--- processes/ProcessENOSE2KG.py
+++ processes/ProcessENOSE2KG.py
@@ -4,17 +4,18 @@ from rdflib.namespace import RDF, RDFS
 
 ex = Namespace('http://purl.com/fsmon#')
 g = Graph()
-wb = load_workbook('/home/appuser/NextFlow-KG-Computational-Workflow/ENOSE.xlsx')
+fname='/home/appuser/NextFlow-KG-Computational-Workflow/ENOSE.xlsx'
+wb = load_workbook(fname)
 sheet = wb['Sheet1']
 if 1==1:
     row= list(sheet.rows)[0]
     print(str(row))
 #for row in sheet.iter_rows(min_row=2, values_only=True):
     subject_uri = row[0].value
-    subject_name = row[1].value
+    subject_name =  fname  #row[1].value
     subject = ex[subject_uri]
     g.add((subject, RDF.type, ex[row[0]]))
-    g.add((subject, ex['hasName'], Literal(subject_name)))
+    g.add((subject, ex['hasDatasetName'], Literal(subject_name)))
 
     for i in range(2, len(row)):
         if row[i]:
