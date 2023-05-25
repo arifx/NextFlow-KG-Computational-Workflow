1c27aabbf8b92ce50b2323abe571147f0ef90464
Arif YILMAZ <73948166+arifx@users.noreply.github.com>
2023-05-25 10:45:06 +0200

    python

--- installNextFlow.sh
+++ installNextFlow.sh
@@ -5,3 +5,7 @@ sudo apt-get install -y openjdk-11-jdk
 curl -s https://get.nextflow.io | bash
 sudo mv nextflow /usr/local/bin/
 nextflow -version
+
+sudo apt-get install python3.10
+
+sudo apt-get install python3-pip
